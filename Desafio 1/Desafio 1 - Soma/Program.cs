b21c52b1143fea974370f9fefc11f4060d339b34
fabioaron <fabioaronsantos@gmail.com>
2025-03-17 13:56:38 -0300

    Commit do segundo desafio em código.

---
+++ Desafio 1/Desafio 1 - Soma/Program.cs
@@ -0,0 +1,18 @@
+﻿
+    int indice = 13;
+    int soma = 0;
+    int k = 0;
+
+do
+{
+    k++;
+    soma = soma + k;
+    Console.WriteLine($"O valor da soma é de: {soma}...");
+
+}
+while (k < indice);
+
+Console.ReadKey();
+
+
+
---
+++ Desafio 1/Desafio 2 - Sequencia de Fibonacci/Desafio 2 - Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Escreva um número de elementos iniciar a sequência de Fibonnaci: \b");
+        // Informe no campo o número para determinar o limite do índice da sequência
+        int numeroMaximo = int.Parse(Console.ReadLine());
+        int numero = 0;
+        int proximoNumero = 1;
+       for (int i = 2; i <= numeroMaximo; i++)
+        {
+            int soma = numero + proximoNumero;
+            Console.WriteLine($"O número da sequência é: {soma}. \b");
+            numero = proximoNumero;
+            proximoNumero = soma;
+        }
+      
+    }
+  
+
+}
