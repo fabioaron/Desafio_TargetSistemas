fb780a33001a6c8084410c9f18594b0d8f61f5a3
fabioaron <fabioaronsantos@gmail.com>
2025-03-17 14:31:47 -0300

    Commit do terceiro desafio!!

---
+++ Desafio 1/Desafio 3 - Vetor de faturamento diário/FaturamentoMensalMarço/Dias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaturamentoMensalMarço
+{
+    class Faturamentos
+    {
+        public string dia { get; set; }
+        public double valor { get; set; }
+    }
+}
---
+++ Desafio 1/Desafio 3 - Vetor de faturamento diário/FaturamentoMensalMarço/Program.cs
@@ -0,0 +1,34 @@
+﻿using FaturamentoMensalMarço;
+using Newtonsoft.Json;
+
+
+class Faturamento
+{
+    public double Valor { get;  set; }
+
+    class Faturamentos
+    {
+        public string Dia { get; set; }
+        public double Valor { get; set; }
+    }
+    static void Main()
+    {
+        string caminhoDoArquivo = @"G:\desafio\Desafio_TargetSistemas\Desafio 1\Desafio 3 - Vetor de faturamento diário\marco_fatur.JSON";
+        string json = File.ReadAllText(caminhoDoArquivo);
+
+        List<Faturamento> faturamentos = JsonConvert.DeserializeObject<List<Faturamento>>(json);
+
+        double menorFaturamento = faturamentos.Min(n => n.Valor);
+        double maiorFaturamento = faturamentos.Max(n => n.Valor);
+        double mediaFaturamento = faturamentos.Average(n => n.Valor);
+        double mediaArrendada = Math.Round(mediaFaturamento, 2);
+        int diasAcimaDaMedia = faturamentos.Count(n => n.Valor > mediaFaturamento);
+
+
+        Console.WriteLine($"O valor mínimo do nosso faturamento foi de: {menorFaturamento:C}.");
+        Console.WriteLine($"O maior valor do nosso faturamento foi de: {maiorFaturamento:C}.");
+        Console.WriteLine($"Nossos dias com o maior faturamento diário foram {diasAcimaDaMedia} dias, com a média de: {mediaArrendada:C}!");
+        Console.ReadKey();
+
+    }
+}
