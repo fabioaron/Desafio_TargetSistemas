168aafcb6c4c0b39fa45c9815f1e77accbfce996
fabioaron <fabioaronsantos@gmail.com>
2025-03-17 14:58:54 -0300

    Commit do desafio final.

---
+++ Desafio 1/Desafio 5 - Inverta/Desafio 5 - Inverta/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Insira um texto: \b");
+string texto = Convert.ToString(Console.ReadLine());
+string textoInvertido = "";
+
+for (int i = texto.Length - 1; i >= 0; i--)
+{
+    textoInvertido += texto[i];
+    // A variável textoInvertido será atribuída com os caractéres de texto inversamente ordenados.
+}
+Console.WriteLine($"Seu texto era: {texto}");
+Console.WriteLine($"Agora ele é: {textoInvertido}");
+Console.WriteLine("Invertido :D");
+Console.ReadKey();
